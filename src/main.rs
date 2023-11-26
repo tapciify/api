58d6954bf2b383016f860775bea2d96b9910a4d8
Aleksei Rybin <0xalekseirybin@gmail.com>
2023-11-26 12:11:04 +0700

    Rewrite back to shuttle

--- src/main.rs
+++ src/main.rs
@@ -1,24 +1,10 @@
 pub mod routes;
 
 use routes::create_routes;
-use std::env;
-use std::net::SocketAddr;
 
-#[tokio::main]
-async fn main() {
-    tracing_subscriber::fmt::init();
-
+#[shuttle_runtime::main]
+async fn main() -> shuttle_axum::ShuttleAxum {
     let app = create_routes();
 
-    let addr = SocketAddr::from((
-        [127, 0, 0, 1],
-        env::var("PORT")
-            .map(|port| port.parse().unwrap())
-            .unwrap_or(3000),
-    ));
-    tracing::debug!("listening on {}", addr);
-    axum::Server::bind(&addr)
-        .serve(app.into_make_service())
-        .await
-        .unwrap();
+    Ok(app.into())
 }
